83cd216f91122def3934d333a9d955cbafc40540
Bruno Marcon <55155016+Bruno-Marcon@users.noreply.github.com>
2023-02-11 20:35:11 -0300

    Testing connection db

---
+++ src/database/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const connectDatabase = () =>{
+    console.log("wait conecting to the db")
+
+    mongoose
+    .connect(
+        "mongodb+srv://root:d45s4Ha@cluster0.3j1eop6.mongodb.net/?retryWrites=true&w=majority",
+        {useNewUrlParser: true, useUnifiedTopology: true}
+        )
+        .then(() => console.log("MongoDB Atlas Connected"))
+        .catch((error) => console.log(error))
+}
+
+module.exports = connectDatabase

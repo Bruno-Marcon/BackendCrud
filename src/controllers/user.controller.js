67085d4c48a7bd782a324144ae8b47ad9e80253e
Bruno Marcon <55155016+Bruno-Marcon@users.noreply.github.com>
2023-02-11 20:55:40 -0300

    Created Route Get

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 const userService = require('../services/user.service')
-
+const mongoose = require('mongoose')
 
 const create =  async (req,res) =>{
     const {name,username,email,password,avatar} = req.body
@@ -27,11 +27,30 @@ const create =  async (req,res) =>{
     })
 }
 
+
 const findAll = async(req, res) =>{
     const users = await userService.findAllService()
 
+    if(users.length === 0){
+        return res.status(400).send({message: "There're no registered users "})
+    }
 
-
+    res.send(users)
 }
 
-module.exports = {create, findAll}
+const findById = async(req, res) =>{
+
+    const id = req.params.id
+
+    if(! mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).send({message: "invalid ID"})
+    }
+
+    const user = await userService.findByIdService(id)
+
+    if(!user){
+        return res.status(400).send({message: "User not found"})
+    }
+    res.send(user)
+}
+module.exports = {create, findAll, findById}
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,6 +2,11 @@ const User = require('../models/User')
 
 const createService = (body) => User.create(body)
 
+const findAllService = () => User.find()
+
+const findByIdService = (id) => User.findById(id)
 module.exports = {
-    create,
+    createService,
+    findAllService,
+    findByIdService
 }

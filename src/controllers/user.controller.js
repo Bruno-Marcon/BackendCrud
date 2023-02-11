ef33480d737bbde5122f3872b35a7fef99112973
Bruno Marcon <55155016+Bruno-Marcon@users.noreply.github.com>
2023-02-11 20:32:50 -0300

    connected DB and testing connection

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,24 @@
-const create = (req,res) =>{
+const userService = require('../services/user.service')
+
+
+const create =  async (req,res) =>{
     const {name,username,email,password,avatar} = req.body
 
     if(!name || !username || !email || !password|| !avatar){
         res.status(400).send({message: "submit all fields for registration"})
     }
 
-    
+    const user =  await userService.createService(req.body)
+
+    if(!user){
+        return res.status(400).send({message: "Error creating user"})
+    }
 
 
     res.status(201).send({
         messagem: "User created successfully",
         user:{
+            id: user._id,
             name,
             username,
             email,
@@ -19,4 +27,11 @@ const create = (req,res) =>{
     })
 }
 
-module.exports = {create}
+const findAll = async(req, res) =>{
+    const users = await userService.findAllService()
+
+
+
+}
+
+module.exports = {create, findAll}

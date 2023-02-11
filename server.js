29e395af446fbc9a131a51a2d378bbb3e5952bc2
Bruno Marcon <55155016+Bruno-Marcon@users.noreply.github.com>
2023-02-11 19:49:17 -0300

    refactor: Config Route

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const userRoute = require("./src/routes/user.route")
 const app = express()
+const port = 3000
 
-app.use("/soma",userRoute )
+app.use(express.json())
+app.use("/user",userRoute )
 
-app.listen(3000)
+app.listen(port, () => console.log(`server is running in ${port}`))
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,22 @@
-const soma = (req,res) =>{
-    const soma = 1+1
-    res.send({soma:soma})
+const create = (req,res) =>{
+    const {name,username,email,password,avatar} = req.body
+
+    if(!name || !username || !email || !password|| !avatar){
+        res.status(400).send({message: "submit all fields for registration"})
+    }
+
+    
+
+
+    res.status(201).send({
+        messagem: "User created successfully",
+        user:{
+            name,
+            username,
+            email,
+            avatar,
+        },
+    })
 }
 
-module.exports = {soma}
+module.exports = {create}

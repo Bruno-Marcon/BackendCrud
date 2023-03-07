876d4e35fe278d2906d945cf47397393370fa3c3
Bruno Marcon <55155016+Bruno-Marcon@users.noreply.github.com>
2023-03-06 22:34:48 -0300

    autentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt'
-import { loginService } from '../services/auth.service.js'
+import { loginService, generateToken} from '../services/auth.service.js'
 
 const login = async (req,res) =>{
     
@@ -8,9 +8,19 @@ const login = async (req,res) =>{
     try{
         const user = await loginService(email)
 
-/*         const passwordIsValid = bcrypt.compare, user.password
- */
-        res.send(user)  
+        if(!user){
+            return res.status(404).send({message: "User or password not found"})
+        }
+
+        const passwordIsValid = await bcrypt.compare(password, user.password)
+
+        if(!passwordIsValid){
+            return res.status(400).send({message: "User or password not found"})
+        }
+
+        const token = generateToken(user.id)
+
+        res.send({token})
     }catch(err){
         res.status(500).send(err.message)
     }
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,8 @@
 import User from '../models/User.js'
+import Jwt from 'jsonwebtoken'
 
 const loginService = (email) => User.findOne({email: email}).select("+password")
 
-export {loginService}   
+const generateToken = (id) => Jwt.sign({id: id},process.env.SECRET_JWT, {expiresIn:3600})
+
+export {loginService,generateToken}
